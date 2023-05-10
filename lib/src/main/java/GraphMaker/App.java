eebce0d5b23a3623ace5ff0a566389a9ea97e52b
Evelyn <evieswelch@gmail.com>
2023-05-09 20:01:28 -0600

    Added JavaFx import and got it working with 'gradlew run' NOTE: through windows WSL it causes an error and wont run, but it runs fine through windows terminal

--- lib/src/main/java/GraphMaker/App.java
+++ lib/src/main/java/GraphMaker/App.java
@@ -1,15 +1,36 @@
 package GraphMaker;
 
-/** 
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
  * @author Evelyn Welch
  * 
- * This is the main entry point for the program
- * */
-public class App {
+ *         This is the main entry point for the program
+ */
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+
+		StackPane root = new StackPane();
+		Scene scene = new Scene(root, 300, 250);
+
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		launch(args);
 	}
 
 }
