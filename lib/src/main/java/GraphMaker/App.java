0dee134b9c2e93bbdfc7f03868bf0976dc7413c7
Evelyn <evieswelch@gmail.com>
2023-05-09 19:36:17 -0600

    Intialized a gradle project

---
+++ lib/src/main/java/GraphMaker/App.java
@@ -0,0 +1,15 @@
+package GraphMaker;
+
+/** 
+ * @author Evelyn Welch
+ * 
+ * This is the main entry point for the program
+ * */
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
